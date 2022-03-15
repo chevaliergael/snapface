71c4453fa79f5e84bff39818e87fd56a23877e4c
GaelChevalier <doggyexistence@yahoo.fr>
2022-03-15 12:55:09 +0100

    ajout directive *ngFor + directives [ngStyle] et [ngClass]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,34 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(){
-    this.mySnap = {
-      title: "Truc Muche",
-      description: "Un mec random",
-      imageUrl: "https://www.placecage.com/c/400/400",
-      createdDate: new Date(),
-      snaps: 0,
-      location: "Los Angeles"
-    };
-
-    this.myOtherSnap = {
-      title: "Une inconnue",
-      description: "Une photo générée par une IA",
-      imageUrl: "https://faces-img.xcdn.link/image-lorem-face-296.jpg",
-      createdDate: new Date(),
-      snaps: 0,
-      location: "Inconnu"
-    };
-
-    this.myLastSnap = {
-      title: "Un visage qui n'existe pas",
-      description: "Un autre visage créé par une IA",
-      imageUrl: "https://faces-img.xcdn.link/image-lorem-face-160.jpg",
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title: "Truc Muche",
+        description: "Un mec random",
+        imageUrl: "https://www.placecage.com/c/400/400",
+        createdDate: new Date(),
+        snaps: 100,
+        location: "Los Angeles"
+      },
+      {
+        title: "Une inconnue",
+        description: "Une photo générée par une IA",
+        imageUrl: "https://faces-img.xcdn.link/image-lorem-face-296.jpg",
+        createdDate: new Date(),
+        snaps: 0,
+        location: "Inconnu"
+      },
+      {
+        title: "Un visage qui n'existe pas",
+        description: "Un autre visage créé par une IA",
+        imageUrl: "https://faces-img.xcdn.link/image-lorem-face-160.jpg",
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }
